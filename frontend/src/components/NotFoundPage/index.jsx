91dc233657c301af7bab9cd968d8b53a5dc189ba
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-06-29 15:07:03 -0700

    added not found page component

---
+++ frontend/src/components/NotFoundPage/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import ".styles.css";
+
+function NotFoundPage() {
+    return (
+        <>
+            <h1>404: Page Not Found</h1>
+            <Link to="/">Return to home page</Link>
+        </>
+    )
+};
+
+export default NotFoundPage;
