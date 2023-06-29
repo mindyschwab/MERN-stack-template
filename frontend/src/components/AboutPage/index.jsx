ddbea70f90128518c0578fadccfd7dc51a4837d0
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-06-28 17:03:10 -0700

    set up frontend folder structure and removed placeholder vite app function

---
+++ frontend/src/components/AboutPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+    return (
+        <>
+            <h1>About Page Title</h1>
+            <p>About page content... </p>
+        </>
+    )
+}
+
+export default AboutPage;
