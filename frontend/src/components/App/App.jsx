31a68b1593119cb04cba675038d418c09a6eff75
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-06-29 15:15:56 -0700

    imported not found page to app.js and updated route

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -3,6 +3,7 @@ import Navbar from '../Navbar'
 import HomePage from '../HomePage'
 import AboutPage from '../AboutPage'
 import ProjectsPage from '../ProjectsPage'
+import NotFoundPage from '../NotFoundPage'
 import './App.css'
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
     </Router>
--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import "./styles.css";
 
 // resource for responsive navbar using tailwind and react: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/documentation/react/navbars
 function Navbar() {
