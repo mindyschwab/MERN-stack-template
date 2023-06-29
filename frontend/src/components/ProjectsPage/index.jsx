b94d542a55c16202034cf579b5515771567b8f4b
mindy schwabenbauer <mindy.schwabenbauer@gmail.com>
2023-06-28 17:34:16 -0700

    added projects page component

---
+++ frontend/src/components/ProjectsPage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ProjectsPage() {
+    return (
+        <>
+            <h1>Projects Page</h1>
+            <p>projects page content... </p>
+        </>
+    )
+}
